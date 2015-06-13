937853fea09eb2f7f8c74a1ea31124857bdff1f2
Fredrik Emilsson <fredrik.emilsson@live.se>
2015-06-13 18:24:53 +0200

    Updated

--- run.R
+++ run.R
@@ -4,6 +4,7 @@ setwd("F:\\Documents\\Developing Data Products\\Developing-Data-Products-Project
 library(shiny)
 runApp(display.mode='showcase')
 
-plot(c(15,35),rep(0.1,2),ylim=c(0,1),axes=F,xlab="",ylab="",type="l",col="red")
-lines(c(15,30),rep(0.1,2),col="green")
-axis(side=1)
+devtools::install_github('rstudio/shinyapps')
+library(shinyapps)
+
+deployApp()
