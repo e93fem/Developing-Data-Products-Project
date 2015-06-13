38285a5717c85e23ea872a4252d40908a553ac4a
Fredrik Emilsson <fredrik.emilsson@live.se>
2015-06-13 17:34:38 +0200

    commit

--- ui.R
+++ ui.R
@@ -5,6 +5,7 @@ shinyUI(
     headerPanel("Calculate BMI"),
     
     sidebarPanel(
+      "Just change the length or weight to get the BMI value!",
       numericInput(inputId="length", label = "Input length in cm", 175),
       numericInput(inputId="weight", label = "Input weight in kg", 70)
     ),
